80b89a14e5b4d05a6af7666950e801cbb7975049
Jisan Zaman <jzthegreat@gmail.com>
2014-03-04 18:40:26 -0500

    scope fb access fixed

--- app/controllers/facebook.js
+++ app/controllers/facebook.js
@@ -9,11 +9,10 @@ exports.showFriend = function(req, res){
     var secretAccess = req.user.fb_accessToken;
     graph.setAccessToken(secretAccess)
     var ownerId = req.user.fb_id
-    var friendsData = [];
-    graph.get(ownerId + '/friends/?fields=id,name,birthday', function(err,data){
-      console.log(data)
+
+    graph.get(ownerId + '/friends/?fields=id,name,birthday,picture', function(err,data){
+        console.log(data)
     })
-    console.log(friendsData)
 
     res.redirect('/');
 };
--- app/routes/users.js
+++ app/routes/users.js
@@ -23,7 +23,7 @@ module.exports = function(app, passport) {
 
     // Setting the facebook oauth routes
     app.get('/auth/facebook', passport.authenticate('facebook', {
-        scope: ['email', 'user_about_me'],
+        scope: ['email', 'user_about_me', 'friends_birthday'],
         failureRedirect: '/signin'
     }), users.signin);
 
