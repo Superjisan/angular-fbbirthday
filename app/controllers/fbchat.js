faace92b7c8d1bdfc13520f238bd4a6cdc81a90d
Jisan Zaman <jzthegreat@gmail.com>
2014-03-11 18:20:49 -0400

    a little better design got birthday list to go horizontally

--- app/controllers/fbchat.js
+++ app/controllers/fbchat.js
@@ -4,80 +4,70 @@ var mongoose = require('mongoose'),
     Birthday = mongoose.model('Birthday'),
     Client = require('node-xmpp-client'),
     User = mongoose.model('User'),
-    ltx  = require('ltx');
+    ltx  = require('ltx'),
+    async = require('async');
 
 
 exports.FbMessage = function (req, res) {
 
-  var facebookId = req.user.fb_id;
-  var user_name = req.user.name;
-   // Eric
-  var friend_name = req.body.friend_name.text();
-  console.log(friend_name)
-  var friend_id;
-  var message = req.body.message;
-  console.log(message)
+  async.waterfall([
+    function(callback) {
 
-  //get and set the friend's facebook id
-  Birthday.findOne({"name" : friend_name}, function(err, data){
-    if (err){console.log(err)}
-    console.log(data)
-  })
+      var friend_id;
+      var friend_name = req.body.friend_name;
 
+      var message = req.body.message;
+      console.log(message)
 
-  var client = new Client({
-    jid : '-' + facebookId + '@chat.facebook.com',
-    api_key : '739502299401603',
-    secret_key : '8efe4a94ec9e73705edad1c56e709e11',
-    access_token : req.user.fb_accessToken
-  });
+      //get and set the friend's facebook id
+      Birthday.findOne({"name" : friend_name}, function(err, data){
+        if (err){console.log(err)}
+         console.log(data)
+         friend_id = data.fb_id
+          callback(null, friend_id, friend_name, message)
+      })
 
 
-    client.addListener('online', function(data) {
-        console.log('Connected as ' + data.jid.user + '@' + data.jid.domain + '/' + data.jid.resource)
 
-        //send message via facebook
-        // var chat = new ltx.Element('message', { to: '-' + friend_id + '@chat.facebook.com', type: 'chat' })
-        //     .c('body')
-        //     .t(message)
-        // client.send(chat)
-    // nodejs has nothing left to do and will exit
-        console.log (user_name + "sent this text '" + message + "' to " + friend_name);
-        client.end()
-    })
+    },
+    function (friend_id, friend_name, message, callback) {
+      var facebookId = req.user.fb_id;
+      var user_name = req.user.name;
 
-    client.on('stanza', function(stanza) {
+      console.log(friend_name, "friend_id: ", friend_id)
 
-        console.log('Received:', stanza.toString())
-    })
+      var client = new Client({
+        jid : '-' + facebookId + '@chat.facebook.com',
+        api_key : '739502299401603',
+        secret_key : '8efe4a94ec9e73705edad1c56e709e11',
+        access_token : req.user.fb_accessToken
+      });
 
-    client.on('connect', function () {
-        console.log('Client is connected')
-    })
+      console.log(req.user.fb_accessToken)
 
-    client.on('reconnect', function () {
-        console.log('Client reconnects …')
-    })
+      client.addListener('online', function(data) {
+          console.log('Connected as ' + data.jid.user + '@' + data.jid.domain + '/' + data.jid.resource)
 
-    client.on('disconnect', function (e) {
-    console.log('Client is disconnected', client.connection.reconnect, e)
-    })
+          //send message via facebook
+          var chat = new ltx.Element('message', { to: '-' + friend_id + '@chat.facebook.com', type: 'chat' })
+              .c('body')
+              .t(message)
 
-    client.addListener('error', function(e) {
-        console.error(e)
-        process.exit(1)
-    })
+          client.send(chat);
+          client.end()
 
-      console.log("I am on fbchat page")
+      // nodejs has nothing left to do and will exit
+          console.log (user_name + " sent this text '" + message + "' to " + friend_name);
 
-    process.on('exit', function () {
-      client.end()
-    })
+        });
 
-    res.render('index', {
-        user: req.user ? JSON.stringify(req.user) : 'null',
-        friend: {id : friend_id, name: friend_name}
-    })
 
+      callback(null, 'done');
+      }
+    ], function(err, result) {
+       if (err) {console.log(err)}
+          res.redirect('/#!/birthdays')
+        })
 }
+
 
