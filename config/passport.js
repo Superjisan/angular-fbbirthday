4c07bbc04b21976eeaf0c1c4ffbf4e9509947f8a
Jisan Zaman <jzthegreat@gmail.com>
2014-03-13 14:21:08 -0400

    took out requirements

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,11 @@
 
 var mongoose = require('mongoose'),
     LocalStrategy = require('passport-local').Strategy,
-    TwitterStrategy = require('passport-twitter').Strategy,
+    // TwitterStrategy = require('passport-twitter').Strategy,
     FacebookStrategy = require('passport-facebook').Strategy,
-    GitHubStrategy = require('passport-github').Strategy,
-    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-    LinkedinStrategy = require('passport-linkedin').Strategy,
+    // GitHubStrategy = require('passport-github').Strategy,
+    // GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+    // LinkedinStrategy = require('passport-linkedin').Strategy,
     User = mongoose.model('User'),
     config = require('./config');
 
