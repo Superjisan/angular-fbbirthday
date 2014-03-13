34ee55e95133bb736b8b68473e6f0c57c594550e
Jisan Zaman <jzthegreat@gmail.com>
2014-03-13 13:32:00 -0400

    update to remove karma, again

--- gruntfile.js
+++ gruntfile.js
@@ -55,30 +55,30 @@ module.exports = function(grunt) {
                 logConcurrentOutput: true
             }
         },
-        mochaTest: {
-            options: {
-                reporter: 'spec',
-                require: 'server.js'
-            },
-            src: ['test/mocha/**/*.js']
-        },
+        // mochaTest: {
+        //     options: {
+        //         reporter: 'spec',
+        //         require: 'server.js'
+        //     },
+        //     src: ['test/mocha/**/*.js']
+        // },
         env: {
             test: {
                 NODE_ENV: 'test'
             }
         },
-        karma: {
-            unit: {
-                configFile: 'test/karma/karma.conf.js'
-            }
-        }
+        // karma: {
+        //     unit: {
+        //         configFile: 'test/karma/karma.conf.js'
+        //     }
+        // }
     });
 
     //Load NPM tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-karma');
+    // grunt.loadNpmTasks('grunt-mocha-test');
+    // grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-env');
@@ -90,5 +90,5 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['jshint', 'concurrent']);
 
     //Test task.
-    grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+    // grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 };
