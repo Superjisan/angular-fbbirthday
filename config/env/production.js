ec4851825433c643522f9a359ba457e462dfd8cf
Jisan Zaman <jzthegreat@gmail.com>
2014-03-13 16:51:06 -0400

    still trying

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://heroku_app22998139:gnwS4etz9m5kgO8HLHnkRCZusjzxYkIB@ds033679.mongolab.com:33679',
+    db: 'mongodb://heroku_app22918578:5umji2jstcd9pfobanm3pfebf2@ds033499.mongolab.com:33499',
     app: {
         name: 'Facebook-Birthday Angular App'
     },
